2fb470f7ccc9488ebe2b830ea1844ac81656664a
Tim Johnson <timjohnsonpdx0@gmail.com>
2020-11-22 09:14:10 -0800

    Cleanup

--- QLearningMaze/QLearningMaze.Ui.Forms/MazeMain.cs
+++ QLearningMaze/QLearningMaze.Ui.Forms/MazeMain.cs
@@ -130,6 +130,8 @@
                 
         private void RespawnMaze()
         {
+            if (_overrideRespawn) return;
+
             var newSpace = new MazeSpace();
 
             SetMazeValuesFromForm();
